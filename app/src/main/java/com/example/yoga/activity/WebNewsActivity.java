8a809b4f70575ec8a5616da63e6b45b275300a30
MarijaSadhana <marijasadhana@gmail.com>
2020-02-07 12:02:59 +0100

    videos and images added WebNewsActivity

---
+++ app/src/main/java/com/example/yoga/activity/WebNewsActivity.java
@@ -0,0 +1,39 @@
+package com.example.yoga.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import com.example.yoga.R;
+
+public class WebNewsActivity extends AppCompatActivity {
+
+    public static final String URL_EXTRA = "URL_EXTRA";
+    WebView webView;
+    TextView error;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_news);
+
+        String url = getIntent().getStringExtra(URL_EXTRA);
+        webView = (WebView) findViewById(R.id.webViewID);
+        error = (TextView) findViewById(R.id.error);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        if (url != null){
+            webView.loadUrl(url);
+        } else {
+            error.setVisibility(View.VISIBLE);
+            webView.setVisibility(View.INVISIBLE);
+        }
+    }
+}
