69e92ce78dae012728adac4d426365bd03f34ba7
MarijaSadhana <marijasadhana@gmail.com>
2020-02-27 10:57:46 +0100

    Alarm

---
+++ app/src/main/java/com/example/yoga/activity/AlarmReceiver.java
@@ -0,0 +1,41 @@
+package com.example.yoga.activity;
+
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.yoga.R;
+import com.example.yoga.db.LocalData;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    String TAG = "AlarmReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction() != null && context != null) {
+            if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
+                // Set the alarm here
+                Log.d(TAG, "onReceive: BOOT_COMPLETED");
+                LocalData localData = new LocalData(context);
+                NotificationScheduler.setReminder(context, AlarmReceiver.class, localData.get_hour(), localData.get_min());
+                return;
+            }
+        }
+
+        Log.d(TAG, "onReceive: ");
+
+        //Trigger the notification
+        NotificationScheduler.showNotification(context, SetAlarm.class,
+                "ЈОГА!", "Издвој време за себеси!");
+
+    }
+}
--- app/src/main/java/com/example/yoga/activity/SetAlarm.java
+++ app/src/main/java/com/example/yoga/activity/SetAlarm.java
@@ -1,62 +1,146 @@
 package com.example.yoga.activity;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.ClipboardManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import com.example.yoga.R;
+import com.example.yoga.db.LocalData;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class SetAlarm extends AppCompatActivity {
 
-    TimePicker timePicker;
-    Button startAlarmBtn;
-    TextView textView;
+    String TAG = "RemindMe";
+    LocalData localData;
+    Switch reminderSwitch;
+    TextView tvTime;
+    LinearLayout ll_set_time;
+    int hour, min;
+    ClipboardManager myClipboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
 
-        startAlarmBtn = findViewById(R.id.btn_startAlarm);
-        timePicker = findViewById(R.id.timePicker);
-        textView = findViewById(R.id.txtAlarmSet);
+        localData = new LocalData(getApplicationContext());
+        myClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ll_set_time = (LinearLayout) findViewById(R.id.ll_set_time);
+        tvTime = (TextView) findViewById(R.id.tv_reminder_time_desc);
+
+        reminderSwitch = (Switch) findViewById(R.id.timerSwitch);
+
+        hour = localData.get_hour();
+        min = localData.get_min();
+
+        tvTime.setText(getFormatedTime(hour, min));
+        reminderSwitch.setChecked(localData.getReminderStatus());
+
+        if (!localData.getReminderStatus())
+            ll_set_time.setAlpha(0.4f);
+
+        reminderSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                localData.setReminderStatus(isChecked);
+                if (isChecked) {
+                    Log.d(TAG, "onCheckedChanged: true");
+                    NotificationScheduler.setReminder(SetAlarm.this, AlarmReceiver.class, localData.get_hour(), localData.get_min());
+                    ll_set_time.setAlpha(1f);
+                } else {
+                    Log.d(TAG, "onCheckedChanged: false");
+                    NotificationScheduler.cancelReminder(SetAlarm.this, AlarmReceiver.class);
+                    ll_set_time.setAlpha(0.4f);
+                }
+
+            }
+        });
+
+        ll_set_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (localData.getReminderStatus())
+                    showTimePickerDialog(localData.get_hour(), localData.get_min());
+            }
+        });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
 
-    public void startAlarm(View view) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH),
-                timePicker.getHour(),
-                timePicker.getMinute(),
-                0
-        );
-        setAlarm(calendar.getTimeInMillis());
+    private void showTimePickerDialog(int h, int m) {
+
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.timepicker_header, null);
+
+        TimePickerDialog builder = new TimePickerDialog(this, R.style.DialogTheme,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int min) {
+                        Log.d(TAG, "onTimeSet: hour " + hour);
+                        Log.d(TAG, "onTimeSet: min " + min);
+                        localData.set_hour(hour);
+                        localData.set_min(min);
+                        tvTime.setText(getFormatedTime(hour, min));
+                        NotificationScheduler.setReminder(SetAlarm.this, AlarmReceiver.class, localData.get_hour(), localData.get_min());
+
+                    }
+                }, h, m, false);
+
+        builder.setCustomTitle(view);
+        builder.show();
+
     }
 
-    private void setAlarm(long timeInMillis) {
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, AlarmBroadcastManager.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,0, intent,0);
-        if (alarmManager != null) {
-            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
+    public String getFormatedTime(int h, int m) {
+        final String OLD_FORMAT = "HH:mm";
+        final String NEW_FORMAT = "hh:mm a";
+
+        String oldDateString = h + ":" + m;
+        String newDateString = "";
+
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(OLD_FORMAT, getCurrentLocale());
+            Date d = sdf.parse(oldDateString);
+            sdf.applyPattern(NEW_FORMAT);
+            newDateString = sdf.format(d);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        Toast.makeText(getApplicationContext(),"Алармот е активиран", Toast.LENGTH_SHORT).show();
+
+        return newDateString;
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    public Locale getCurrentLocale() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return getResources().getConfiguration().getLocales().get(0);
+        } else {
+            //noinspection deprecation
+            return getResources().getConfiguration().locale;
+        }
     }
 }
